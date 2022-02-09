ccfd85a668220eb754f1e2391a3869a3af709602
Nuri Korganci <nkorganci@gmail.com>
2022-02-08 23:55:23 -0600

    m

--- 4.7 RGB Color.py
+++ 4.7 RGB Color.py
@@ -2,3 +2,8 @@
 # Red =0-256
 # https://www.csfieldguide.org.nz/en/interactives/rgb-mixer/
 # Pixels: are shown by colors
+
+a ="Harmony"
+for i in range(len(a)):
+    print("My " + str(i+1) +". character is "  + a[i])
+
